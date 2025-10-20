11c355c9a6162ec1211d4f2ea1c752563d298f31
Esteban Lopez Perez <estlopper@alum.us.es>
2025-10-20 19:10:52 +0200

    tests de integración, de interfaz  y de locust

--- app/modules/notepad/tests/locustfile.py
+++ app/modules/notepad/tests/locustfile.py
@@ -6,14 +6,46 @@ class NotepadBehavior(TaskSet):
     def on_start(self):
         self.index()
 
-    @task
+    @task(2)
     def index(self):
         response = self.client.get("/notepad")
 
         if response.status_code != 200:
             print(f"Notepad index failed: {response.status_code}")
 
-
+    @task(1)
+    def create_notepad(self):
+        response = self.client.post("/notepad/create", json={"title": "Notepad generado por Locust", "body": "Contenido de prueba"})
+        if response.status_code == 201:
+            print("Notepad creado correctamente.")
+        else:
+            print(f"Error al crear el notepad: {response.status_code}")
+            
+    @task(1)
+    def get_notepad(self):
+        response = self.client.get("/notepad/1")
+        if response.status_code == 200:
+            print("Notepad obtenido correctamente.")
+        else:
+            print(f"Error al obtener el notepad: {response.status_code}")
+            
+    @task(1)
+    def edit_notepad(self):
+        response = self.client.post("/notepad/edit/1", json={"title": "Notepad editado por Locust", "body": "Contenido editado de prueba"})
+        if response.status_code == 200:
+            print("Notepad editado correctamente.")
+        else:
+            print(f"Error al editar el notepad: {response.status_code}")
+        
+    @task(1)
+    def delete_notepad(self):
+        response = self.client.post("/notepad/delete/1")
+        if response.status_code == 204:
+            print("Notepad eliminado correctamente.")
+        else:
+            print(f"Error al eliminar el notepad: {response.status_code}")
+            
+            
 class NotepadUser(HttpUser):
     tasks = [NotepadBehavior]
     min_wait = 5000
--- app/modules/notepad/tests/test_selenium.py
+++ app/modules/notepad/tests/test_selenium.py
@@ -1,4 +1,5 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 import time
 
 from core.environment.host import get_host_for_selenium_testing
@@ -13,7 +14,28 @@ def test_notepad_index():
         host = get_host_for_selenium_testing()
 
         # Open the index page
-        driver.get(f'{host}/notepad')
+        # driver.get(f'{host}/notepad')
+        driver.get("http://127.0.0.1:5000/login?next=%2Fnotepad")
+        driver.set_window_size(1850, 1053)
+        driver.find_element(By.ID, "email").click()
+        driver.find_element(By.ID, "password").send_keys("1234")
+        driver.find_element(By.ID, "email").send_keys("user1@example.com")
+        driver.find_element(By.ID, "submit").click()
+        driver.get("http://127.0.0.1:5000/notepad/create")
+        driver.find_element(By.ID, "title").click()
+        driver.find_element(By.ID, "title").send_keys("Prueba")
+        driver.find_element(By.ID, "body").send_keys("Prueba")
+        driver.find_element(By.ID, "submit").click()
+        driver.find_element(By.LINK_TEXT, "Edit").click()
+        driver.find_element(By.ID, "title").click()
+        driver.find_element(By.ID, "title").send_keys("Prueba editada")
+        driver.find_element(By.ID, "body").click()
+        driver.find_element(By.ID, "body").send_keys("Prueba editada")
+        driver.find_element(By.CSS_SELECTOR, ".content").click()
+        driver.find_element(By.ID, "submit").click()
+        driver.find_element(By.CSS_SELECTOR, "button").click()
+        driver.find_element(By.CSS_SELECTOR, ".sidebar-item:nth-child(10) .align-middle:nth-child(2)").click()
+        driver.close()
 
         # Wait a little while to make sure the page has loaded completely
         time.sleep(4)
